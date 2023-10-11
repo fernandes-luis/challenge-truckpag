5255c7ad6100e775c27e05ffd06f06ab11396f95
Luis Fernandes <lf.moraes21gbi@gmail.com>
2023-10-11 00:27:58 -0300

    Initial Cron

---
+++ app/Console/Commands/ProductsCron.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ProductsCron extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'product:cron';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
